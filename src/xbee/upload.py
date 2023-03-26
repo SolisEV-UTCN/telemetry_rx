ec4b318b0b3a5b8d3ebb2c6058b2b47c5d6301a5
Nemeth Raymond <nraymi@gmail.com>
2023-03-26 12:39:03 +0300

    Removed comments

--- src/xbee/upload.py
+++ src/xbee/upload.py
@@ -15,29 +15,3 @@ for x in range(50):
     p = Point("my_measurement").tag("location", "Solis").field("test", x)
     write_api.write(bucket=bucket, record=p)
     time.sleep(3)
-
-
-# query
-# from(bucket: "solis_telemetry")
-# | > range(start: v.timeRangeStart, stop: v.timeRangeStop)
-# | > filter(fn: (r)=>
-#            r._measurement == "my_measurement" and
-#            r._field == "test"
-#            )
-
-# query for filter by value
-# // v.bucket, v.timeRangeStart, and v.timeRange stop are all variables supported by the flux plugin and influxdb
-# from(bucket: v.bucket)
-#   |> range(start: v.timeRangeStart, stop: v.timeRangeStop)
-#   |> filter(fn: (r) => r["_value"] >= 10 and r["_value"] <= 20)
-
-
-# query for group by
-
-# // v.windowPeriod is a variable referring to the current optimized window period (currently: $interval)
-# from(bucket: v.bucket)
-#   |> range(start: v.timeRangeStart, stop: v.timeRangeStop)
-#   |> filter(fn: (r) => r["_measurement"] == "measurement1" or r["_measurement"] =~ /^.*?regex.*$/)
-#   |> filter(fn: (r) => r["_field"] == "field2" or r["_field"] =~ /^.*?regex.*$/)
-#   |> aggregateWindow(every: v.windowPeriod, fn: mean|median|max|count|derivative|sum)
-#   |> yield(name: "some-name")
