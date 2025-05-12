36a25d2114aec3480872df8d0394523b59ab1350
vladmarian20005 <78913181+vladmarian20005@users.noreply.github.com>
2025-05-12 18:25:35 +0300

    Update usb_adapter.py

--- src/telemetry_rx/adapters/usb_adapter.py
+++ src/telemetry_rx/adapters/usb_adapter.py
@@ -16,7 +16,7 @@ class UsbAdapter(Adapter):
         super().__init__(*args, **kwargs)
         self.running = True
         self.address = address
-        self.baudrate = 230400
+        self.baudrate = 115200
         self.bytesize = serial.EIGHTBITS
         self.parity = serial.PARITY_EVEN
         self.stopbits = serial.STOPBITS_ONE
